218a0ef16b9c20f80873b22fc3640d0b672e91ac
Ramil <ramik_96_08@mail.ru>
2025-01-02 14:26:43 +0600

    add react virtualized

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -5,7 +5,9 @@ import { HTMLAttributeAnchorTarget, memo } from "react";
 import { EArticleView, IArticle } from "../../model/types/article";
 import { ArticleListItem } from "../ArticleListItem/ArticleListItem";
 import { ArticleListItemSkeleton } from "../ArticleListItem/ArticleListItem.skeleton";
-import { Text, TextSize, TextTheme } from "@/shared/ui/Text/Text";
+import { Text, TextSize } from "@/shared/ui/Text/Text";
+import { List, ListRowProps, WindowScroller } from "react-virtualized";
+import { PAGE_ID } from "@/widgets/Page/Page";
 
 interface ArticleListProps {
   className?: string;
@@ -33,15 +35,34 @@ export const ArticleList = memo((props: ArticleListProps) => {
   } = props;
   const { t } = useTranslation("articles");
 
-  const renderArticle = (article: IArticle) => {
+  const isBig = view === EArticleView.BIG;
+
+  const itemPerRow = isBig ? 1 : 6;
+  const rowCount = isBig
+    ? articles.length
+    : Math.ceil(articles.length / itemPerRow);
+
+  const rowRender = ({ index, isScrolling, key, style }: ListRowProps) => {
+    const items = [];
+    const fromIndex = index * itemPerRow;
+    const toIndex = Math.min(fromIndex + itemPerRow, articles.length);
+
+    for (let i = fromIndex; i < toIndex; i++) {
+      items.push(
+        <ArticleListItem
+          target={target}
+          article={articles[i]}
+          view={view}
+          className={cls.card}
+          key={"str" + i}
+        />
+      );
+    }
+
     return (
-      <ArticleListItem
-        target={target}
-        article={article}
-        view={view}
-        className={cls.card}
-        key={article.id}
-      />
+      <div key={key} style={style} className={cls.row}>
+        {items}
+      </div>
     );
   };
 
@@ -54,10 +75,34 @@ export const ArticleList = memo((props: ArticleListProps) => {
   }
 
   return (
-    <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-      {articles.length ? articles.map(renderArticle) : null}
-      {isLoading && getSkeletons(view)}
-    </div>
+    <WindowScroller scrollElement={document.getElementById(PAGE_ID) as Element}>
+      {({
+        height,
+        width,
+        registerChild,
+        onChildScroll,
+        isScrolling,
+        scrollTop,
+      }) => (
+        <div
+          ref={registerChild as React.LegacyRef<HTMLDivElement>}
+          className={classNames(cls.ArticleList, {}, [className, cls[view]])}
+        >
+          <List
+            height={height ?? 700}
+            rowCount={rowCount}
+            rowHeight={isBig ? 700 : 330}
+            rowRenderer={rowRender}
+            width={width ? width - 80 : 700}
+            autoHeight
+            onScroll={onChildScroll}
+            isScrolling={isScrolling}
+            scrollTop={scrollTop}
+          />
+          {isLoading && getSkeletons(view)}
+        </div>
+      )}
+    </WindowScroller>
   );
 });
 ArticleList.displayName = "ArticleList";
